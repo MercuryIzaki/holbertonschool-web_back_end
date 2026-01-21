be5fba76857226da4c02f42e77df53958d59d60f
MercuryIzaki <Izakimercury@gmail.com>
2026-01-21 12:35:13 +0000

    Add task 12: Python script for Nginx log stats

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Script that provides some stats about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+
+
+def log_stats():
+    """ Provides stats about Nginx logs """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    # Общее количество логов
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    # Статистика по методам
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    # Статистика по проверке статуса (GET на /status)
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
+
+
+if __name__ == "__main__":
+    log_stats()
